dcb657205c9aed947a7aa858ead61dd4c7dcef6c
kairosia <kang970917@gmail.com>
2026-01-30 16:42:18 +0900

    Title: 1부터 특정 수까지의 합, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260130/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
+++ 260130/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
@@ -1,8 +1,11 @@
 n = int(input())
 
 # Please write your code here.
-def sum_divide_10(n):
+def sum_div_10(n):
     sum = 0
-    for i in range(n):
+    for i in range(n+1):
         sum += i
+
     return sum // 10
+
+print(sum_div_10(n))
