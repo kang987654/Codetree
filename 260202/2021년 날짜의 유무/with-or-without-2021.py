7586cb21ec09b49b2404485bfad359ac21702c9e
kairosia <kang970917@gmail.com>
2026-02-02 15:45:58 +0900

    Title: 2021년 날짜의 유무, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260202/2021년 날짜의 유무/with-or-without-2021.py
+++ 260202/2021년 날짜의 유무/with-or-without-2021.py
@@ -6,7 +6,7 @@ def is_valid_day(M, D):
         return True
     if M in [1, 3, 5, 7, 8, 10, 12] and 0 < D < 32:
         return True
-    if M in [2, 4, 6, 9, 11] and 0 < D < 31:
+    if M in [4, 6, 9, 11] and 0 < D < 31:
         return True
     return False
 
