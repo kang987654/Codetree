c837783abc39ec4f6a5184c07529680e10c908fa
kairosia <kang970917@gmail.com>
2026-02-02 15:33:33 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260202/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,18 @@
+n1, n2 = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+# Please write your code here.
+def is_in(a, b):
+    st_a, st_b = '', ''
+    for w in a:
+        st_a += str(w)
+    for w in b:
+        st_b += str(w)
+    
+    return st_b in st_a
+
+if is_in(a, b):
+    print('Yes')
+else:
+    print('No')
