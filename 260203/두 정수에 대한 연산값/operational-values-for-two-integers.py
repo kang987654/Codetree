211ec795ac1394e11e5a7ffcd7221002d2f32a2a
kairosia <kang970917@gmail.com>
2026-02-03 10:32:10 +0900

    Title: 두 정수에 대한 연산값, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 260203/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 260203/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -2,7 +2,10 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 def mul_or_add(a, b):
-    return min(a, b)*2, max(a, b)+25
+    if a < b:
+        return a*2, b+25
+    else:
+        return a+25, b*2
 
 x, y = mul_or_add(a, b)
 print(x, y)
