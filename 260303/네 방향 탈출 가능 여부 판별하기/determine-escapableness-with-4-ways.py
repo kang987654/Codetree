010197420437cf61b074177432c2c64055507c2d
kairosia <kang970917@gmail.com>
2026-03-03 02:56:16 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 82ms, Memory: 19MB, Status: Passed - Codetree

---
+++ 260303/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+from collections import deque
+
+dr = [-1, +1, +0, +0]
+dc = [+0, +0, -1, +1]
+
+visited = [[False] * m for _ in range(n)]
+visited[0][0] = True
+
+stack = deque([(0, 0)])
+
+while stack:
+    x, y = stack.popleft()
+
+    for di, dj in zip(dr, dc):
+        next_x, next_y = x + di, y + dj
+        if 0 <= next_x < n and 0 <= next_y < m and \
+        a[next_x][next_y] and (not visited[next_x][next_y]):
+            visited[next_x][next_y] = True
+            stack.append((next_x, next_y))
+
+print(int(visited[n-1][m-1]))
