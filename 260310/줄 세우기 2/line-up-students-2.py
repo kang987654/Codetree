739a63573cc2356e63b3e85f51aa3cdc93752cda
kairosia <kang970917@gmail.com>
2026-03-10 09:11:48 +0900

    Title: 줄 세우기 2, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260310/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+students = [
+    (h, w, i + 1)
+    for i, (h, w) in enumerate([tuple(map(int, input().split())) for _ in range(n)])
+]
+
+# Please write your code here.
+students.sort(key=lambda student: (student[0], -student[1]))
+for student in students:
+    print(*student)
