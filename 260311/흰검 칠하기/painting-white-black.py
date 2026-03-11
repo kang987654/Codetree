582c1275a3c3345d422bdd788e531f8c911218b9
kairosia <kang970917@gmail.com>
2026-03-11 10:59:15 +0900

    Title: 흰검 칠하기, Time: 68ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 260311/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,38 @@
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+# Please write your code here.
+state = [0] * 200001
+color = ['N'] * 200001
+now = 100000
+for i in range(n):
+    if dir[i] == 'L':
+        now += 1
+        for _ in range(x[i]):
+            now -= 1
+            
+            state[now] += 100
+            color[now] = 'W'
+    else:
+        now -= 1
+        for _ in range(x[i]):
+            now += 1
+            
+            state[now] += 1
+            color[now] = 'B'
+
+w, b, g = 0, 0, 0
+for i, s in enumerate(state):
+    if s >= 202:
+        g += 1
+    elif color[i] == 'W':
+        w += 1
+    elif color[i] == 'B':
+        b += 1
+
+print(w, b, g)
