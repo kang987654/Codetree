e12f2818105cb12c8647015b0ff7c812c791e9d0
kairosia <kang970917@gmail.com>
2026-03-12 09:15:16 +0900

    Title: 사각형의 총 넓이 2, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260312/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -0,0 +1,19 @@
+n = int(input())
+x1, y1, x2, y2 = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    x1.append(a)
+    y1.append(b)
+    x2.append(c)
+    y2.append(d)
+
+# Please write your code here.
+offset = 100
+
+new_plane = [[0] * 200 for _ in range(200)]
+for r1, c1, r2, c2 in zip(x1, y1, x2, y2):
+    for r in range(r1 + offset, r2 + offset):
+        for c in range(c1 + offset, c2 + offset):
+            new_plane[r][c] = 1
+
+print(sum(map(sum, new_plane)))
