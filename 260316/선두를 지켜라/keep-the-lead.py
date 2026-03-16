afb0b4c0edf719d584a1a7613f6e87708d30dfb1
kairosia <kang970917@gmail.com>
2026-03-16 14:29:45 +0900

    Title: 선두를 지켜라, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260316/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,42 @@
+n, m = map(int, input().split())
+
+# Process A's movements
+v = []
+t = []
+for _ in range(n):
+    vi, ti = map(int, input().split())
+    v.append(vi)
+    t.append(ti)
+
+# Process B's movements
+v2 = []
+t2 = []
+for _ in range(m):
+    vi, ti = map(int, input().split())
+    v2.append(vi)
+    t2.append(ti)
+
+# Please write your code here.
+A, B = [], []
+for vi, ti in zip(v, t):
+    A += [vi] * ti
+for vi, ti in zip(v2, t2):
+    B += [vi] * ti
+
+a, b = 0, 0
+i = 0
+prev = 's'
+cnt = 0
+while i < sum(t):
+    a += A[i]
+    b += B[i]
+    i += 1
+
+    if a > b and prev != 'a':
+        cnt += 1
+        prev = 'a'
+    if a > b and prev != 'b':
+        cnt += 1
+        prev = 'b'
+
+print(cnt)
