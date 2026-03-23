313dac349de2319ae837478a35ffcde0f5fa626c
kairosia <kang970917@gmail.com>
2026-03-23 17:29:06 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260323/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -0,0 +1,34 @@
+n = int(input())
+grid = [[0] * n for _ in range(n)]
+
+# Please write your code here.
+dr = [+0, +1, +0, -1]
+dc = [+1, +0, -1, +0]
+
+d = 0
+r, c = n // 2, n // 2
+
+i = 1
+_len = 1
+while i < n*n:
+    for _ in range(_len):
+        grid[r][c] = i
+        i += 1
+        r += dr[d]
+        c += dc[d]
+    d = (d+4 - 1) % 4
+
+    if i >= n*n:
+        break
+
+    for _ in range(_len):
+        grid[r][c] = i
+        i += 1
+        r += dr[d]
+        c += dc[d]
+    d = (d+4 - 1) % 4
+
+    _len += 1
+
+for row in grid:
+    print(*row)
