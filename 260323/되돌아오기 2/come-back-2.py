2195637b3bd8082789512d8ea0e6573217dbb8bf
kairosia <kang970917@gmail.com>
2026-03-23 14:47:28 +0900

    Title: 되돌아오기 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260323/되돌아오기 2/come-back-2.py
@@ -0,0 +1,28 @@
+commands = input()
+
+# Please write your code here.
+dir = [
+    (+0, +1),
+    (+1, +0),
+    (+0, -1),
+    (-1, +0)
+]
+
+x, y = 0, 0
+d = 0
+t = 0
+for command in commands:
+    t += 1
+    if command == 'F':
+        dx, dy = dir[d]
+        x += dx
+        y += dy
+    if command == 'R':
+        d = (d+1) % 4
+    if command == 'L':
+        d = (d+4-1) % 4
+
+    if x == 0 and y == 0:
+        break
+
+print(t)
