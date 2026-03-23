faeb0c49ccc5facf403c4f99ed29fb8abe8d8814
kairosia <kang970917@gmail.com>
2026-03-23 16:51:39 +0900

    Title: 거울에 레이저 쏘기 2, Time: 62ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 260323/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 260323/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -10,18 +10,17 @@ head = {
     'R': (+0, +1)
 }
 
-
 if k <= n:
     laser_r, laser_c = 0, k - 1
     laser_d = 'D'
-elif k <= 2*n:
-    laser_r, laser_c = k - (n+1), n - 1
+elif k <= 2 * n:
+    laser_r, laser_c = k - (n + 1), n - 1
     laser_d = 'L'
-elif k <= 3*n:
-    laser_r, laser_c = n - 1, 3*n - k
+elif k <= 3 * n:
+    laser_r, laser_c = n - 1, 3 * n - k
     laser_d = 'U'
 else:
-    laser_r, laser_c = 4*n - k, 0
+    laser_r, laser_c = 4 * n - k, 0
     laser_d = 'R'
 
 cnt = 0
@@ -36,7 +35,7 @@ while 0 <= laser_r < n and 0 <= laser_c < n:
             laser_d = 'D'
         if laser_d == 'R':
             laser_d = 'U'
-    if grid[laser_r][laser_c] == '\\':
+    else:   # grid[laser_r][laser_c] == '\\':
         if laser_d == 'U':
             laser_d = 'L'
         if laser_d == 'D':
@@ -45,7 +44,7 @@ while 0 <= laser_r < n and 0 <= laser_c < n:
             laser_d = 'U'
         if laser_d == 'R':
             laser_d = 'D'
-        
+
     dr, dc = head[laser_d]
     laser_r += dr
     laser_c += dc
