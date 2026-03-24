c66c4914fd833d36f7c41d54237a8500dc1a8e4f
kairosia <kang970917@gmail.com>
2026-03-24 10:42:43 +0900

    Title: 체크판위에서 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260324/체크판위에서 2/on-the-checkboard-2.py
+++ 260324/체크판위에서 2/on-the-checkboard-2.py
@@ -3,20 +3,24 @@ grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
 start = grid[0][0]
+end = grid[R-1][C-1]
 
-first = []
-for i in range(1, R-1):
-    for j in range(1, C-1):
-        if grid[i][j] != start:
-            first.append((i, j))
+if start == end:
+    print(0)
+else:
+    first = []
+    for i in range(1, R-1):
+        for j in range(1, C-1):
+            if grid[i][j] != start:
+                first.append((i, j))
 
-cnt = 0
-while first:
-    r, c = first.pop()
+    cnt = 0
+    while first:
+        r, c = first.pop()
 
-    for i in range(r+1, R-1):
-        for j in range(c+1, C-1):
-            if grid[i][j] == start:
-                cnt += 1
+        for i in range(r+1, R-1):
+            for j in range(c+1, C-1):
+                if grid[i][j] == start:
+                    cnt += 1
 
-print(cnt)
+    print(cnt)
