e72040c7ffe94b44750fde083ba757951d43890b
kairosia <kang970917@gmail.com>
2026-03-30 16:56:47 +0900

    Title: G or H 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260330/G or H 2/G-or-H-2.py
+++ 260330/G or H 2/G-or-H-2.py
@@ -4,8 +4,6 @@ pos = [int(p[0]) for p in people]
 alpha = [p[1] for p in people]
 
 # Please write your code here.
-# 최대 100 개 이므로 g 1 h 100 이면
-# 101로 나누어 쌍이 맞는지 확인
 alpha = [1 if a == 'G' else 100 for a in alpha]
 people = sorted(zip(pos, alpha), key=lambda pa: pa[0])
 
@@ -13,7 +11,10 @@ s_alpha = [p[1] for p in people]
 def max_pair():
     for size in range(n, 1, -1):
         for i in range(n+1 - size):
-            if sum(s_alpha[i:i+size]) % 101 == 0:
+            # G 로만 or H 로만 or 정확히 같은 개수
+            if 1 < sum(s_alpha[i:i+size]) < 100 or \
+            (100 < sum(s_alpha[i:i+size]) and sum(s_alpha[i:i+size]) % 100 == 0) or \
+            sum(s_alpha[i:i+size]) % 101 == 0:
                 l = i
                 r = i+size-1
 
