5a24663a220adf5e6dfca8f1f58ad37f99d4fe46
kairosia <kang970917@gmail.com>
2026-03-30 12:52:19 +0900

    Title: G or H 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/G or H 3/G-or-H-3.py
+++ 260330/G or H 3/G-or-H-3.py
@@ -12,7 +12,10 @@ for i, s in zip(x, c):
     scores[i-1] = 1 if s == 'G' else 2
 
 answer = 0
-for i in range(len(scores)-k):
-    answer = max(sum(scores[i:i+k+1]), answer)
+if len(scores) > k:
+    for i in range(len(scores)-k):
+        answer = max(sum(scores[i:i+k+1]), answer)
+else:
+    answer = sum(scores)
 
 print(answer)
