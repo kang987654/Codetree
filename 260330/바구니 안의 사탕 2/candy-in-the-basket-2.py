ddd16272422f7a4bc734ccf9360eb1b8898da7f1
kairosia <kang970917@gmail.com>
2026-03-30 15:33:02 +0900

    Title: 바구니 안의 사탕 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260330/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+candy = []
+pos = []
+
+for _ in range(N):
+    c, p = map(int, input().split())
+    candy.append(c)
+    pos.append(p)
+
+# Please write your code here.
+bucket = [0] * max(pos)
+for i in range(N):
+    bucket[pos[i]-1] = candy[i]
+
+answer = 0
+for i in range(K, len(bucket)-K+1):
+    answer = max(sum(bucket[i-K:i+K+1]), answer)
+
+print(answer)
