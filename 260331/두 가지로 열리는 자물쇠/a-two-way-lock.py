bc9d29605fb2fd3ab1f572e6f2df6d42a3e0de7b
kairosia <kang970917@gmail.com>
2026-03-31 11:54:42 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 260331/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -9,13 +9,13 @@ else:
     answer = 5*5*5 * 2
 
     numbers = [N] + list(range(1, N))
-    c_a1 = set([numbers[a1-2], numbers[a1-1], numbers[a1], numbers[(a1+1) % N], numbers[(a1+2) % N]])
-    c_b1 = set([numbers[b1-2], numbers[b1-1], numbers[b1], numbers[(b1+1) % N], numbers[(b1+2) % N]])
-    c_c1 = set([numbers[c1-2], numbers[c1-1], numbers[c1], numbers[(c1+1) % N], numbers[(c1+2) % N]])
+    c_a1 = set([numbers[a1-2], numbers[a1-1], numbers[a1 % N], numbers[(a1+1) % N], numbers[(a1+2) % N]])
+    c_b1 = set([numbers[b1-2], numbers[b1-1], numbers[b1 % N], numbers[(b1+1) % N], numbers[(b1+2) % N]])
+    c_c1 = set([numbers[c1-2], numbers[c1-1], numbers[c1 % N], numbers[(c1+1) % N], numbers[(c1+2) % N]])
 
-    c_a2 = set([numbers[a2-2], numbers[a2-1], numbers[a2], numbers[(a2+1) % N], numbers[(a2+2) % N]])
-    c_b2 = set([numbers[b2-2], numbers[b2-1], numbers[b2], numbers[(b2+1) % N], numbers[(b2+2) % N]])
-    c_c2 = set([numbers[c2-2], numbers[c2-1], numbers[c2], numbers[(c2+1) % N], numbers[(c2+2) % N]])
+    c_a2 = set([numbers[a2-2], numbers[a2-1], numbers[a2 % N], numbers[(a2+1) % N], numbers[(a2+2) % N]])
+    c_b2 = set([numbers[b2-2], numbers[b2-1], numbers[b2 % N], numbers[(b2+1) % N], numbers[(b2+2) % N]])
+    c_c2 = set([numbers[c2-2], numbers[c2-1], numbers[c2 % N], numbers[(c2+1) % N], numbers[(c2+2) % N]])
     
     dup = len(c_a1 & c_a2) * len(c_b1 & c_b2) * len(c_c1 & c_c2)
 
